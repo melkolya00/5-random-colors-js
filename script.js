19d5b2c9f484e30a2c22d1bcd69df1a948e0e220
Николай Мелеховец <melehkola@gmail.com>
2023-07-26 18:03:13 +0300

    new variant of styling

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 const columns = document.querySelectorAll(".col")
-
-
-function generateRandomColors (){
-    const hexCodes = '0123456789ABCDEF'
-    let color = "";
-    for (let i = 0; i < 6; i++){
-        color += hexCodes[Math.floor(Math.random()*hexCodes.length)]
+document.addEventListener('keydown', event => {
+    if (event.code.toLowerCase() === 'space') {
+        event.preventDefault();
+        setRandomColors();
+        document.querySelector('#hint').style.display = 'none';
     }
-    return '#' + color
+});
+
+
+document.addEventListener ('click', event => {
+    const type = event.target.dataset.type;
+    if (type === 'lock'){
+        const elem = event.target.children[0]
+        elem.classList.toggle('fa-lock')
+        elem.classList.toggle('fa-unlock')
+    }
 }
+)
+// function generateRandomColors (){
+//     const hexCodes = '0123456789ABCDEF'
+//     let color = "";
+//     for (let i = 0; i < 6; i++){
+//         color += hexCodes[Math.floor(Math.random()*hexCodes.length)]
+//     }
+//     return '#' + color
+// }
+function generateRandomColors() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
+}
+
 function setRandomColors(){
     columns.forEach(col=>{
-        const textName = col.querySelector('h2');
-        textName.textContent = generateRandomColors()
-        col.style.background = generateRandomColors()
+        const isLocked = col.querySelector('i').classList.contains('fa-lock');
+        const text = col.querySelector('h2');
+        const button = col.querySelector('button');
+        const color = generateRandomColors();
+if (isLocked) {return}
+        text.textContent = color;
+        col.style.background = color;
+        setTextColor (text, color)
+        setTextColor (button, color)
     })
 }
 
+function setTextColor (text, color){
+    const luminance = chroma(color).luminance()
+    text.style.color = luminance >0.5 ? 'black' : 'white'
+}
 setRandomColors()
